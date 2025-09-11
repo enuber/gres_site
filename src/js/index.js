8fb6298be566017a14490a4cb9fca731d5b26fcd
Erik Nuber <erik.nuber@yahoo.com>
2025-09-11 16:41:37 -0700

    added in hero and about sections to home page

--- src/js/index.js
+++ src/js/index.js
@@ -23,29 +23,3 @@ if (isWebpAvailable.classList.contains('webp')) {
     elem.dataset.src = singleSrc;
   });
 }
-
-const activeNav = document.querySelector('.navigation__active');
-
-activeNav.addEventListener('click', (e) => {
-  e.preventDefault();
-});
-
-//  hiding part of nav or showing it. Otherwise see the buttons on the hamburger nav transition visibly on screen.
-const setNavStyles = () => {
-  const navigation = document.querySelector('.navigation__nav');
-  const checkBox = document.querySelector('.navigation__checkbox');
-  if (checkBox.checked) {
-    checkBox.checked = false;
-  }
-  if (window.innerWidth < 600) {
-    navigation.style.display = 'none';
-    setTimeout(() => {
-      navigation.style.display = 'block';
-    }, 200);
-  } else {
-    // navigation.style.display = 'none';
-    // setTimeout(() => {
-    //   navigation.style.display = 'block';
-    // }, 200);
-  }
-};
